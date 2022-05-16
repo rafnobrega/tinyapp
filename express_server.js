37ffea913eac280f29616e6cfc9e7e755078068b
Rafael Nobrega <rafael.rlnr@gmail.com>
2022-05-16 15:57:02 -0700

    urls_show.ejs created with basic function to show one single shortened URL form.

--- express_server.js
+++ express_server.js
@@ -15,6 +15,11 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/:shortURL", (req, res) => {
+  const templateVars = { shortURL: req.params.shortURL, longURL: req.params.longURL };
+  res.render("urls_show", templateVars);
+});
+
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
