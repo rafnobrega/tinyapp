bad8e78665a66ad97af15ee2aa4982e222a53d5a
Rafael Nobrega <rafael.rlnr@gmail.com>
2022-05-16 17:35:52 -0700

    body-parser installed and set

--- express_server.js
+++ express_server.js
@@ -5,14 +5,29 @@ const PORT = 8080; // default port 8080
 //This code sets EJS as the view engine for the Express application using:
 app.set("view engine", "ejs");
 
+// // Generate a Random ShortURL - this is a simulation only
+// function generateRandomString() {
+
+// }
+
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com",
 };
 
+// The body-parser library will convert the request body from a Buffer into string that we can read. It will then add the data to the req(request) object under the key body.
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
+});
+
+// ##### NEW URL PAGE ##### //
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
 });
 
 app.get("/urls/:shortURL", (req, res) => {
@@ -20,6 +35,7 @@ app.get("/urls/:shortURL", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
+// ##### HOME  ##### //
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
@@ -41,6 +57,14 @@ app.get("/set", (req, res) => {
 app.get("/fetch", (req, res) => {
   res.send(`a = ${a}`);
 });
+
+// define the route that will match this POST request and handle it. 
+//Let's start with a simple definition that logs the request body and gives a dummy response.
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+});
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
