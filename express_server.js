ac24ffce4221fdbd4a44ea74a595a0929f681fbe
Rafael Nobrega <rafael.rlnr@gmail.com>
2022-05-17 23:28:36 -0700

    register - POST created; userDB created

--- express_server.js
+++ express_server.js
@@ -5,32 +5,50 @@ const PORT = 8080; // default port 8080
 // ##### VIEW ENGINE ##### //
 app.set("view engine", "ejs");
 
-// Generate a Random ShortURL - this is a simulation only
+// #### RANDOM shortURL GENERATOR ##### // 
 function generateRandomString() {
   let str = Math.random().toString(36).slice(2, 8);
   console.log("THIS IS MY STR:", str);
   return str;
 }
 
+
+// ##### 📀 URL DB 📀 ##### //
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com",
+  "a1b2c3": "http://www.rafnobrega.com"
 };
 
 
+// ##### 💿 USER DB 💿 ##### //
+const userDatabase = {
+  userRandomID: {
+    id: "userRandomID",
+    email: "user@example.com",
+    password: "purple-monkey-dinosaur",
+  },
+  user2RandomID: {
+    id: "user2RandomID",
+    email: "user2@example.com",
+    password: "dishwasher-funk",
+  },
+};
+console.log("MY USER DATABASE IS HERE $$$", userDatabase);
 
 // ##### 🍪 COOKIE-PARSER SETUP ##### //
 const cookieParser = require("cookie-parser");
 app.use(cookieParser());
 
 
-// ##### BODY-PARSER SETUP ##### //
+// ##### 🧍‍♂️ BODY-PARSER SETUP ##### //
 const bodyParser = require("body-parser");
 const { response } = require("express");
 app.use(bodyParser.urlencoded({ extended: true }));
 
+
+// ##### INDEX PAGE ##### //
 app.get("/urls", (req, res) => {
-  console.log("REQ. COOKIES:", req.signedCookies);
   const templateVars = { 
     username: req.cookies["username"],
     urls: urlDatabase 
@@ -47,6 +65,7 @@ app.get("/urls/new", (req, res) => {
   res.render("urls_new", templateVars);
 });
 
+// ##### SHOW URL ##### //
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = {
     shortURL: req.params.shortURL,
@@ -75,13 +94,42 @@ app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
+// ##### 🍏 REGISTER - GET 🍏 ##### //
+app.get("/register", (req, res) => {
+  const templateVars = {
+    username: req.cookies["username"],
+  };
+  res.render("register", templateVars);
+})
+
+// ##### 🍎 REGISTER - POST 🍎 ##### //
+app.post("/register", (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  const id = generateRandomString();
+  // console.log("EMAIL ===>:", email)
+  // console.log("PASSWORD ===>:", password);
+  // console.log("ID ===>:", id);
+  const newUser = {
+    id,
+    email,
+    password
+  };
+  // console.log("USER ===>:", newUser);
+  userDatabase[id] = {id, email, password};
+  console.log("USERDATABASE ===>:", userDatabase);
+
+  res.cookie("username", email);  
+  res.cookie("user_id", id);
+  res.redirect(`/urls`);
+})
 
 
 // ##### LOGIN ##### //
 app.post("/login", (req, res) => {
   const email = req.body.username;
   if (!email) {
-    return res.status(401).send("Unauthorized access - Please go back and provide a valid email address.");
+    return res.status(401).send("Unauthorized access - Please go back.");
   } 
    res.cookie("username", email);
    res.redirect(`/urls`);
@@ -97,7 +145,6 @@ app.post("/logout", (req, res) => {
 // ##### CREATE NEW URL ##### //
 app.post("/urls", (req, res) => {
   // Log the POST request body to the console
-  console.log("## CREATE NEW URL - req.body ##", req.body);
   const longer = req.body.longURL;
   const shorter = generateRandomString(longer);
   urlDatabase[shorter] = longer;
@@ -115,11 +162,7 @@ app.post("/urls/:shortURL/delete", (req, res) => {
 // ##### EDIT AN URL ##### //
 app.post("/urls/:shortURL", (req, res) => {
   const longer = req.body.longURL;
-  console.log("## req.body.id ##", req.body.longURL);
-  console.log("## longer variable ##", req.body);
-  console.log("## longer ##", longer);
   urlDatabase[req.params.shortURL] = longer;
-
   return res.redirect("/urls");
 });
 
